d03f170d2b7e6e916e3b8ff00b67a1f3afdba2b8
henryjameslau <henry.james.lau@gmail.com>
2020-10-29 10:35:35 +0000

    legend from config

--- small-multiple/data/config.js
+++ small-multiple/data/config.js
@@ -1,5 +1,6 @@
 var config = {
   "breaksAll": [-1000, -10, -5, -2, 0, 2, 5, 10, 1000],
+  "legendBreaks":["10% and above","5% to 9.9%","2% to 4.9%","0% to 1.9%","-2% to 0%","-4.9% to -2%","-9.9% to -5%","-10% and below"],
   "colour": [
     "#fff",
     "#8c510a",
@@ -11,10 +12,10 @@ var config = {
     "#35978f",
     "#01665e"
   ],
-  margin:{
-    top:55,
-    right:10,
-    bottom:10,
-    left:10
+  "margin":{
+    "top":55,
+    "right":10,
+    "bottom":10,
+    "left":10
   }
 };
