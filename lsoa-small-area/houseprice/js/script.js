0b91697a552554bd802b0c3a89caab68b079b8ab
henryjameslau <henry.james.lau@gmail.com>
2020-10-30 10:41:37 +0000

    Mobile styling

--- lsoa-small-area/houseprice/js/script.js
+++ lsoa-small-area/houseprice/js/script.js
@@ -238,25 +238,7 @@ if (Modernizr.webgl) {
     map.on('mouseleave', 'lsoa-boundaries', onLeave);
 
 
-    map.on('click', 'lsoa-boundaries', onClick)
-
-
-
-
-    // function tog(v) {
-    //   return v ? 'addClass' : 'removeClass';
-    // }
-    //
-    // $(document).on('input', '.clearable', function() {
-    //   $(this)[tog(this.value)]('x');
-    // }).on('mousemove', '.x', function(e) {
-    //   $(this)[tog(this.offsetWidth - 28 < e.clientX - this.getBoundingClientRect().left)]('onX');
-    // }).on('touchstart click', '.onX', function(ev) {
-    //   ev.preventDefault();
-    //   $(this).removeClass('x onX').val('').change();
-    //   enableMouseEvents();
-    //   onLeave();
-    // });
+    map.on('click', 'lsoa-boundaries', onClick);
 
     function onClick(e) {
       disableMouseEvents();
